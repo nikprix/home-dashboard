7aa0e2c96e29946911ad02d29de7eb2d50d93316
nikprix <gitkola@gmail.com>
2017-06-03 00:48:43 -0400

    fixing error 500

--- app/Init.php
+++ app/Init.php
@@ -1,4 +1,4 @@
 <?php
 
-require_once  'core/App.php';
-require_once  'core/Controller.php';
+require_once  $_SERVER['DOCUMENT_ROOT'].'/home_dashboard/app/core/App.php';
+require_once  $_SERVER['DOCUMENT_ROOT'].'/home_dashboard/app/core/Controller.php';
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,12 +4,12 @@ class Controller
 {
     public function model($model)
     {
-        require_once '../app/models/' . $model . '.php';
+        require_once $_SERVER['DOCUMENT_ROOT'].'/home_dashboard/app/models/' . $model . '.php';
         return new $model();
     }
 
     public function view($view)
     {
-        require_once '../app/views/' . $view . '.php';
+        require_once $_SERVER['DOCUMENT_ROOT'].'/home_dashboard/app/views/' . $view . '.php';
     }
 }
