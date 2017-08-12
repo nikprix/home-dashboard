43b531e309a87baf87f8ac44cf40a902bdc5b182
nikprix <gitkola@gmail.com>
2017-08-12 01:52:11 -0400

    enabling weather

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,6 +1,6 @@
 $(function () {
     clock();
-    //meteo();
+    meteo();
     getPicture();
     getCalendar();
 
@@ -520,12 +520,16 @@ window.setInterval(function () {
 }, 60000); // 1 minute
 
 window.setInterval(function () {
-    //meteo();
+    meteo();
 }, 600000); // 10 minutes
 
 window.setInterval(function () {
     getPicture();
 }, 300000); // 5 minutes
+
+window.setInterval(function () {
+    getPicture();
+}, 900000); // 15 minutes
 
 
 
