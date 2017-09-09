581642a6970705fd3b6be2a7a993e00d9db2eca0
nikprix <gitkola@gmail.com>
2017-09-09 15:41:10 -0400

    Added motion sensor logic

--- app/python/pir.py
+++ app/python/pir.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import sys
 import time
@@ -6,8 +6,9 @@ import RPi.GPIO as io
 import subprocess
 
 io.setmode(io.BCM)
-SHUTOFF_DELAY = 60 # seconds
-PIR_PIN = 4       # 7 on the board
+SHUTOFF_DELAY = 60  # seconds
+PIR_PIN = 4  # 7 on the board
+
 
 def main():
     io.setup(PIR_PIN, io.IN)
@@ -17,6 +18,7 @@ def main():
     while True:
         if io.input(PIR_PIN):
             last_motion_time = time.time()
+            print("Motion detected!")  ### DEBUG
             print ".",
             sys.stdout.flush()
             if turned_off:
@@ -26,15 +28,16 @@ def main():
             if not turned_off and time.time() > (last_motion_time + SHUTOFF_DELAY):
                 turned_off = True
                 turn_off()
-            if not turned_off and time.time() > (last_motion_time + 1):
-            # io.output(LED_PIN, io.HIGH)
         time.sleep(.1)
+
 
 def turn_on():
     subprocess.call("sh /home/pi/Home_Dashboard/power_Screen_ON.sh", shell=True)
 
+
 def turn_off():
     subprocess.call("sh /home/pi/Home_Dashboard/power_Screen_OFF.sh", shell=True)
+
 
 if __name__ == '__main__':
     try:
