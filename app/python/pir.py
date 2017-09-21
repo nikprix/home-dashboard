db7cf6f0629791a8f8f0d7a41894b652d6980fea
nikprix <gitkola@gmail.com>
2017-09-21 11:21:57 -0400

    Added Pythons schedule library to handle switching HDMI on

--- app/python/pir.py
+++ app/python/pir.py
@@ -15,7 +15,7 @@ PIR_PIN = 4  # 7 on the board
 
 ### Time below is used for defining time range withing which we power HDMI off
 timeStart = datetime.time(0, 01, 0)
-timeEnd = datetime.time(5, 59, 15)
+timeEnd = datetime.time(5, 59, 30)
 timeSwitchOn =  "5:59"
 
 
